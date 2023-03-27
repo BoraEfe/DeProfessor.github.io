955e75118070e9afd78442065e40e1249c5dfdbd
BoraEfe <Bora.e@icloud.com>
2023-03-27 13:48:45 +0200

    siktergitcommiet
    daan = homo

--- pages/inlog.php
+++ pages/inlog.php
@@ -6,14 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>De Professor</title>
-    <link rel="stylesheet" href="/css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
 
 <body>
-    <form name="inlog" action="inlog2.php" method="post">
-        <p>voer username in <input type="text" id="username" name="username"></p>
-        <p>voer wachtwoord in <input type="password" id="password" name="password"></p>
-        <p><input type="submit" name="verstuur" value="verzend"></p>
-    </form>
+    <div class="inlog-block">
+        <div class="inlog-menu-welkom">
+            welkom
+        </div>
+        <div class="inlog-menu-welkom2">
+            Log in
+        </div>
+        <form name="inlog" action="inlog2.php" method="post">
+            <p>voer username in <input type="text" id="username" name="username"></p>
+            <p>voer wachtwoord in <input type="password" id="password" name="password"></p>
+            <p><input type="submit" id="verzend" name="verstuur" value="verzend"></p>
+        </form>
+        <div class="goback" onclick="location.href='../index.php'">
+            Terug
+        </div>
+    </div>
 
     <body>
 
--- pages/menu.php
+++ pages/menu.php
@@ -6,23 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
     <script src="../menuToggle.js"></script>
     <script src="https://kit.fontawesome.com/a2d23ebfd3.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <div class="header-content">
-        <img class="michelin1" src="/Images/michelin.png">
-        <img class="michelin2" src="/Images/michelin.png">
-        <img class="michelin3" src="/Images/michelin.png">
+        <img class="michelin1" src="../Images/michelin.png">
+        <img class="michelin2" src="../Images/michelin.png">
+        <img class="michelin3" src="../Images/michelin.png">
         <div class="header-li">
             <ul>
                 <li>MENU</li>
             </ul>
         </div>
         <div class="foldmenu" onclick="toggleMenu()">
-            <img class="menu-icon" src="Images/menu-icon.svg" with="30px" height="40px">
+            <img class="menu-icon" src="../Images/menu-icon.svg" with="30px" height="40px">
         </div>
         <div id="slideMenu">
             <p class="menuButtonText" onclick="location.href='../index.php'">home</p>
@@ -35,17 +35,35 @@
         </div>
     </div>
     <nav class=menu-itemlist>
-
-
     </nav>
     <div class="menu-body">
-        <div class=itembox>
-            <div class="itembox-info">
-                <i class="fa-solid fa-plus fa-1x"></i>
+        <?php
+        require "conn.php";
+        $stmt = $conn->query("SELECT * FROM products");
+        while ($row = $stmt->fetch()) {
+            echo '
+            <div class="itembox">
+            <br>
+            <br>
+            <br>
+            <br>
+             <img src="' . $row['image'] . '" width="170px" height="50px">
+                <div class="itembox-info">
+                ' . $row['product_name'] . '
+                ' . $row['product_info'] . '
+                ' . $row['prijs'] . '   
+                    <i class="fa-solid fa-plus fa-1x"></i>
+                </div>
             </div>
-        </div>
+            ';
+        }
+
+
+   ?>
+        
 
     </div>
+    
     </div>
 </body>
 
