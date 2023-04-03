0f764f4ca2e2bbf313d257d22a5621c11c13133f
BoraEfe <Bora.e@icloud.com>
2023-04-03 11:08:31 +0200

    lucas
    n

--- pages/inlog2.php
+++ pages/inlog2.php
@@ -6,5 +6,5 @@
         $sql = "INSERT INTO users (username, password) VALUES (' $username  ', '  $password ')";
        $conn->exec($sql);
        // echo $username . " " . $password;        header('Location: editor.php')   
-        header('Location: ../editor.php')
+        header('Location: /editor.php')
      ?>
