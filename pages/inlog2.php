dc3690fe5cee93d04c1a7fe395ae86d32031f701
BoraEfe <Bora.e@icloud.com>
2023-04-05 13:33:57 +0200

    blabla
    f

--- pages/inlog2.php
+++ pages/inlog2.php
@@ -1,10 +1,22 @@
 
 <?php
       require_once 'conn.php';
+      var_dump($_POST);
         $username = $_POST['username']; 
         $password = $_POST['password'];
-        $sql = "INSERT INTO users (username, password) VALUES (' $username  ', '  $password ')";
-       $conn->exec($sql);
+        $sql = "SELECT * FROM users WHERE username=:name AND password=:word";
+        $stmt = $conn-> prepare($sql);
+        $stmt->execute(['name' => $username, 'word' => $password]); 
+
+        $user = $stmt->fetch();
        // echo $username . " " . $password;        header('Location: editor.php')   
-        header('Location: editor.php')
+        header('Location: editor.php');
+        
+        if($user)
+        {
+          session_start();
+        }
+        else(
+          header('location: index.php')
+        )
      ?>
--- pages/register2.php
+++ pages/register2.php
@@ -4,6 +4,6 @@
         $password = $_POST['password'];
         $sql = "INSERT INTO users (username, password) VALUES (' $username  ', '  $password ')";
        $conn->exec($sql);
-       // echo $username . " " . $password;        header('Location: editor.php')   
-        header('Location: login.php')
+       // echo $username . " " . $password;        header('Location: login.php')   
+        header('Location: inlog.php');
      ?>
